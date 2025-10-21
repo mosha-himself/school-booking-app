fe2664528d85c6484647cfdd710e7ee7e8ee3b9f
Muhammad Shazeb <mshazebalam@gmail.com>
2025-10-21 22:19:00 +0400

    Ready for Render deploy

--- app.py
+++ app.py
@@ -742,3 +742,10 @@ def admin_export():
 # -------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use Render's port, default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
